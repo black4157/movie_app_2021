bab08d6cf97061848366150c6555ae4175116dd5
nose <yellow4157@naver.com>
2021-08-20 16:54:36 +0900

    upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+function Food({name, Image}){
+  return (
+    <div>
+      <h1>I love {name}</h1>
+      <img src={Image} alt={name} />
+    </div>
+  );
+}
+
+const foodILike = [
+  {
+    id:1,
+    name: "kimchi",
+    Image: "https://health.chosun.com/site/data/img_dir/2020/07/21/2020072103114_0.jpg"
+  },
+  {
+    id:2,
+    name: "gogi",
+    Image: "https://cdn.mindgil.com/news/photo/202104/71169_7768_150.jpg"
+  }
+]
+
+function renderFood(dish){
+  return <Food key={dish.id} name={dish.name} Image={dish.Image} />
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {console.log(foodILike.map(renderFood))}
+      {foodILike.map(renderFood)}
     </div>
   );
 }
